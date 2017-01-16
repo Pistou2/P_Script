a2cbb44fba493fbef4598306aa85000f41d83be9
Yann Merk <yannmerk@gmail.com>
2017-01-16 10:25:44 +0100

    Added the brainstorm and the userAct UML diagram
    + the .gitignore

---
+++ Documentation/brainstorm.mm
@@ -0,0 +1,126 @@
+<map version="freeplane 1.5.9">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="P_Script" FOLDED="false" ID="ID_1270182491" CREATED="1484297632259" MODIFIED="1484297647033" STYLE="oval">
+<font SIZE="18"/>
+<hook NAME="MapStyle" zoom="1.61">
+    <properties fit_to_viewport="false;"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" COLOR="#000000" STYLE="fork">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10.0 pt" SHAPE_VERTICAL_MARGIN="10.0 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+<edge COLOR="#ff0000"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+<edge COLOR="#0000ff"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+<edge COLOR="#00ff00"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+<edge COLOR="#ff00ff"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5">
+<edge COLOR="#00ffff"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6">
+<edge COLOR="#7c0000"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7">
+<edge COLOR="#00007c"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8">
+<edge COLOR="#007c00"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9">
+<edge COLOR="#7c007c"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10">
+<edge COLOR="#007c7c"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11">
+<edge COLOR="#7c7c00"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="9" RULE="ON_BRANCH_CREATION"/>
+<node TEXT="Powershell" POSITION="right" ID="ID_804542458" CREATED="1484297696225" MODIFIED="1484298173994">
+<edge COLOR="#00007c"/>
+<node TEXT="Interface" ID="ID_580757552" CREATED="1484297716682" MODIFIED="1484297723088">
+<node TEXT="Mod&#xe8;le (visio)" ID="ID_1893293978" CREATED="1484297749560" MODIFIED="1484297824941"/>
+</node>
+<node TEXT="UML" ID="ID_939713116" CREATED="1484297859465" MODIFIED="1484297861267">
+<node TEXT="ActUser" ID="ID_1686131227" CREATED="1484297862481" MODIFIED="1484297870185"/>
+<node TEXT="Sequence" ID="ID_267622566" CREATED="1484297870519" MODIFIED="1484297872553"/>
+</node>
+<node TEXT="Code" ID="ID_1207561453" CREATED="1484297828615" MODIFIED="1484297830040">
+<node TEXT="Gestions taille disque" ID="ID_114566160" CREATED="1484297889843" MODIFIED="1484297898852">
+<node TEXT="Graphique" ID="ID_426626905" CREATED="1484297901617" MODIFIED="1484297909114"/>
+</node>
+<node TEXT="Nom PC" ID="ID_1071629602" CREATED="1484297910607" MODIFIED="1484297924581"/>
+<node TEXT="Liste utilisateurs" ID="ID_153771519" CREATED="1484297924812" MODIFIED="1484297930789"/>
+<node TEXT="Liste groupe" ID="ID_271532877" CREATED="1484297931291" MODIFIED="1484297933908"/>
+<node TEXT="Liste service" ID="ID_1046906795" CREATED="1484297934178" MODIFIED="1484297941602">
+<node TEXT="Filtrable" ID="ID_1207940755" CREATED="1484297942673" MODIFIED="1484297945042"/>
+</node>
+<node TEXT="caract&#xe9;ristique PC" ID="ID_1314470832" CREATED="1484297946312" MODIFIED="1484297959654">
+<node TEXT="Ram" ID="ID_1051726369" CREATED="1484297960709" MODIFIED="1484297961973"/>
+<node TEXT="CPU" ID="ID_1284472358" CREATED="1484297962453" MODIFIED="1484297963814"/>
+<node TEXT="CG" ID="ID_1162973600" CREATED="1484297964404" MODIFIED="1484297967254"/>
+<node TEXT="..." ID="ID_73025973" CREATED="1484297967484" MODIFIED="1484297970012"/>
+</node>
+</node>
+<node TEXT="coming Soon:tm:" ID="ID_1628383950" CREATED="1484298131722" MODIFIED="1484298150018"/>
+</node>
+<node TEXT="VMs" POSITION="left" ID="ID_1703704154" CREATED="1484297705442" MODIFIED="1484298357764">
+<edge COLOR="#007c00"/>
+<node TEXT="W7" ID="ID_957848120" CREATED="1484297781245" MODIFIED="1484297782655"/>
+<node TEXT="W8.1" ID="ID_303220761" CREATED="1484297783412" MODIFIED="1484297786644"/>
+<node TEXT="W10" ID="ID_1091092509" CREATED="1484297787704" MODIFIED="1484297789793"/>
+<node TEXT="W serv 2012" ID="ID_420822866" CREATED="1484297790118" MODIFIED="1484298041250" MAX_WIDTH="60.123963254821426 pt" MIN_WIDTH="60.123963254821426 pt"/>
+<node TEXT="W serv 2016" ID="ID_1955403778" CREATED="1484297795626" MODIFIED="1484297805956"/>
+<node TEXT="Linux" ID="ID_1709416877" CREATED="1484297806409" MODIFIED="1484298357760" HGAP_QUANTITY="16.249999932944775 pt" VSHIFT_QUANTITY="2.9999999105930355 pt">
+<node TEXT="Gentoo" ID="ID_599018" CREATED="1484297813164" MODIFIED="1484297815301"/>
+</node>
+<node TEXT="Communication entre VM" ID="ID_646958640" CREATED="1484298002223" MODIFIED="1484298009084"/>
+</node>
+</node>
+</map>
