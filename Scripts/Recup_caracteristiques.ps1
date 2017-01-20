61348ac7b24a5f3b0fd271a58ff89ad8ec941974
Yann Merk <yannmerk@gmail.com>
2017-01-20 11:23:44 +0100

    Worked on computer connection and getting his name

---
+++ Scripts/Recup_caracteristiques.ps1
@@ -0,0 +1,10 @@
+﻿
+#Connection to the computer
+<#TODO#>$password = read-host -assecurestring 
+$username = "W10-1\merkya"
+$cred = new-object -typename System.Management.Automation.PSCredential -argumentlist $username, $password
+
+#Get the computer Name
+$targetComputerName = Invoke-Command -ComputerName 127.0.0.1 -ScriptBlock{$env:COMPUTERNAME} -Credential $cred
+
+echo $targetComputerName
