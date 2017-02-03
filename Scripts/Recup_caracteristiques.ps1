92096caf51bf08bb85df0a1ca94308a1c6fa0784
Yann Merk <yannmerk@gmail.com>
2017-02-03 11:24:00 +0100

    Updated scripts + Created the Project Report

--- Scripts/Recup_caracteristiques.ps1
+++ Scripts/Recup_caracteristiques.ps1
@@ -1,16 +1,40 @@
-﻿
-#Connection to the computer
+﻿#Connection to the computer
 <#USERNAME#>$username = "merkya"
-<#PASSWORD#>$password = read-host -assecurestring 
+<#PASSWORD#>$password = read-host -Prompt "Mot de Passe" -AsSecureString
 <#TARGET IP#>$targetIp = "127.0.0.1"
-<#COMMAND#>$command = 'Get-WmiObject -List *'
 
-#Load the functions
+#Load the function file
 $workingdir = Split-Path $MyInvocation.MyCommand.Path -Parent
 . "$workingdir\functions.ps1"
 
 #Initialize the session
-$session = createSession  -computerIp $targetIp -username $username -password $password
+$session = createSession  -computerIp $targetIp -username $username -password $password <#Password must be a secure string#>
+
+
+<#COMMAND#>$command = 'Get-WmiObject Win32_VideoController'
 
 #send the command
-sendCommand -command $command -session $session | Format-Table
+$commandResult = sendCommand -Command $command <#Script to execute, as a string#> -Session $session <#Previously created session#>
+
+#Simply display the output
+$commandResult | Format-Table
+
+<#=========================
+       Command List 
+===========================
+Computer Name :
+$env:COMPUTERNAME
+
+Ram :
+Get-WmiObject -Class Win32_OperatingSystem -Property TotalVisibleMemorySize, FreePhysicalMemory
+
+GPU :
+Get-WmiObject -Class Win32_VideoController
+
+CPU : 
+Get-WmiObject -Class Win32_Processor
+
+Windows version : 
+Get-WmiObject -Class Win32_OperatingSystem -Property Description, Caption, OSArchitecture, ServicePackMajorVersion, BuildNumber, Version
+
+#>
